ec3b583800fc24176c255e955a4ec9bbb4aba4ef
Harshith VH <vhharshith77@gmail.com>
2022-02-18 02:10:30 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#! python
+
+import cv2
+import numpy
+import torch
+
+# On terminal window - git clone https://github.com/ultralytics/yolov3 
+# On terminal cd yolov3/
+# On terminal pip install -r requirements.txt
+# For images:
+# On terminal python detect.py --source 'image file name'
+# For videos:
+# On terminal python detect.py --source 'video.mp4'
