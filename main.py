7625368df739307f3f685e3ccfb69e2d05a42cf1
Harshith VH <vhharshith77@gmail.com>
2022-02-17 23:41:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import torch
 # On terminal cd yolov3/
 # On terminal pip install -r requirements.txt
 # For images:
-# On terminal python detect.py --source 'image file name'
+# On terminal - python detect.py --source 'image file name'
+# Sample image - https://techlearn-cdn.s3.amazonaws.com/Data%20Science%20Brand%20Sessions/amazon_go/images.jpg
 # For videos:
-# On terminal python detect.py --source 'video.mp4'
+# On terminal - python detect.py --source 'video.mp4'
+# Sample video:  https://techlearn-cdn.s3.amazonaws.com/Data%20Science%20Brand%20Sessions/amazon_go/test_video.mp4
